46549d794ad454b9e10a72ea9bd2f0e02e15483e
earsonlau <earsonlau@gmail.com>
2020-04-18 10:13:44 +0800

    增加"Z字形变换"

---
+++ ARRAY/Z字形变换.py
@@ -0,0 +1,45 @@
+# 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+#
+# 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+#
+# L   C   I   R
+# E T O E S I I G
+# E   D   H   N
+# 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+#
+# 请你实现这个将字符串进行指定行数变换的函数：
+#
+# string convert(string s, int numRows);
+# 示例 1:
+#
+# 输入: s = "LEETCODEISHIRING", numRows = 3
+# 输出: "LCIRETOESIIGEDHN"
+# 示例 2:
+#
+# 输入: s = "LEETCODEISHIRING", numRows = 4
+# 输出: "LDREOEIIECIHNTSG"
+# 解释:
+#
+# L     D     R
+# E   O E   I I
+# E C   I H   N
+# T     S     G
+
+def convert(s: str, numRows: int) -> str:
+    if numRows < 2: return s
+    res = ["" for _ in range(numRows)]
+    print("res:", res)
+    i, flag = 0, -1
+
+    for c in s:
+        print("c:", c)
+        res[i] += c
+        print("res now is :", res)
+        if i == 0 or i == numRows - 1: # 撞到上界或者下界就弹回去
+            flag = -flag # 改变方向
+        print("now the flag is ", flag)
+        i += flag
+
+    return "".join(res)
+
+convert(s="LEETCODEISHIRING", numRows=3)
