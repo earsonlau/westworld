5d4e4b78a13de81bb175d6b25c37b02129651704
earsonlau <earsonlau@gmail.com>
2020-04-19 12:53:16 +0800

    增加"最长有效括号"外观数列"

---
+++ STRING/最长有效括号.py
@@ -0,0 +1,82 @@
+# 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+#
+# 示例 1:
+#
+# 输入: "(()"
+# 输出: 2
+# 解释: 最长有效括号子串为 "()"
+# 示例 2:
+#
+# 输入: ")()())"
+# 输出: 4
+# 解释: 最长有效括号子串为 "()()"
+#
+# 思路一：
+# 代码一：
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        if not s:
+            return 0
+        res = []
+        stack = []
+        for i in range(len(s)):
+            if stack and s[i] == ")":
+                res.append(stack.pop())
+                res.append(i)
+            if s[i] == "(":
+                stack.append(i)
+        res.sort()
+        # print(res)
+        i = 0
+        ans = 0
+        n = len(res)
+        while i < n:
+            j = i
+            while j < n - 1 and res[j + 1] == res[j] + 1:
+                j += 1
+            ans = max(ans, j - i + 1)
+            i = j + 1
+        return ans
+
+
+# 代码二：
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        if not s:
+            return 0
+        res = 0
+        stack = [-1]
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    res = max(res, i - stack[-1])
+        return res
+
+
+# 思路二：
+# 代码三：
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        n = len(s)
+        if n == 0: return 0
+        dp = [0] * n
+        res = 0
+        for i in range(n):
+            if i > 0 and s[i] == ")":
+                if s[i - 1] == "(":
+                    dp[i] = dp[i - 2] + 2
+                elif s[i - 1] == ")" and i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == "(":
+                    dp[i] = dp[i - 1] + 2 + dp[i - dp[i - 1] - 2]
+                if dp[i] > res:
+                    res = dp[i]
+        return res
+
+
