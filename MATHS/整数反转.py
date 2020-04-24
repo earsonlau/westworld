0ee31ed1550fe36ac9c8e430229e34afce8b3890
earsonlau <earsonlau@gmail.com>
2020-04-24 15:10:35 +0800

    增加题解若干

---
+++ MATHS/整数反转.py
@@ -0,0 +1,38 @@
+# # 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+# #
+# # 示例 1:
+# #
+# # 输入: 123
+# # 输出: 321
+# #
+# # 示例 2:
+# #
+# # 输入: -123
+# # 输出: -321
+# #
+# # 示例 3:
+# #
+# # 输入: 120
+# # 输出: 21
+# #
+# # 注意:
+# #
+# # 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。
+# # 请根据这个假设，如果反转后整数溢出那么就返回 0。
+#
+#     1 	class Solution {
+#     2 	    public int reverse(int x) {
+#     3 	        int ans = 0;
+#     4 	        while (x != 0) {
+#     5 	            int pop = x % 10;
+#     6 	            // 溢出判断
+#     7 	            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7))
+#     8 	                return 0;
+#     9 	            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8))
+#    10 	                return 0;
+#    11 	            ans = ans * 10 + pop;
+#    12 	            x /= 10;
+#    13 	        }
+#    14 	        return ans;
+#    15 	    }
+# 	}
---
+++ MATHS/有效数字.py
@@ -0,0 +1,68 @@
+# 验证给定的字符串是否可以解释为十进制数字。
+#
+# 例如:
+#
+# "0" => true
+# " 0.1 " => true
+# "abc" => false
+# "1 a" => false
+# "2e10" => true
+# " -90e3   " => true
+# " 1e" => false
+# "e3" => false
+# " 6e-1" => true
+# " 99e2.5 " => false
+# "53.5e93" => true
+# " --6 " => false
+# "-+3" => false
+# "95a54e53" => false
+#
+# 说明: 我们有意将问题陈述地比较模糊。在实现代码之前，你应当事先思考所有可能的情况。这里给出一份可能存在于有效十进制数字中的字符列表：
+#
+# 数字 0-9
+# 指数 - "e"
+# 正/负号 - "+"/"-"
+# 小数点 - "."
+# 当然，在输入中，这些字符的上下文也很重要。
+#
+# 更新于 2015-02-10:
+# C++函数的形式已经更新了。如果你仍然看见你的函数接收 const char * 类型的参数，请点击重载按钮重置你的代码。
+#
+#
+# 把当前的输入分成几类，再用几个标志位来判断当前是否合法。
+
+
+# public boolean isNumber(String s) {
+#     s = s.trim();
+#
+#     boolean pointSeen = false;
+#     boolean eSeen = false;
+#     boolean numberSeen = false;
+#     boolean numberAfterE = true;
+#     for(int i=0; i<s.length(); i++) {
+#         if('0' <= s.charAt(i) && s.charAt(i) <= '9') {
+#             numberSeen = true;
+#             numberAfterE = true;
+#         } else if(s.charAt(i) == '.') {
+#             if(eSeen || pointSeen) {
+#                 return false;
+#             }
+#             pointSeen = true;
+#         } else if(s.charAt(i) == 'e') {
+#             if(eSeen || !numberSeen) {
+#                 return false;
+#             }
+#             numberAfterE = false;
+#             eSeen = true;
+#         } else if(s.charAt(i) == '-' || s.charAt(i) == '+') {
+#             if(i != 0 && s.charAt(i-1) != 'e') {
+#                 return false;
+#             }
+#         } else {
+#             return false;
+#         }
+#     }
+#
+#     return numberSeen && numberAfterE;
+# }
+
