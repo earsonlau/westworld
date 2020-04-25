44f273aad316c1cae2f2f729d7df3d08ce2f900c
earsonlau <earsonlau@gmail.com>
2020-04-25 11:30:35 +0800

    增加"下一个排列""不同路径"注释

--- ARRAY/下一个排列.py
+++ ARRAY/下一个排列.py
@@ -1,34 +1,14 @@
-# public class Solution {
-#     public void nextPermutation(int[] nums) {
-#         int i = nums.length - 2;
-#         while (i >= 0 && nums[i + 1] <= nums[i]) {
-#             i--;
-#         }
-#         if (i >= 0) {
-#             int j = nums.length - 1;
-#             while (j >= 0 && nums[j] <= nums[i]) {
-#                 j--;
-#             }
-#             swap(nums, i, j);
-#         }
-#         reverse(nums, i + 1);
-#     }
+# 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
 #
-#     private void reverse(int[] nums, int start) {
-#         int i = start, j = nums.length - 1;
-#         while (i < j) {
-#             swap(nums, i, j);
-#             i++;
-#             j--;
-#         }
-#     }
+# 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
 #
-#     private void swap(int[] nums, int i, int j) {
-#         int temp = nums[i];
-#         nums[i] = nums[j];
-#         nums[j] = temp;
-#     }
-# }
+# 必须原地修改，只允许使用额外常数空间。
+#
+# 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+# 1,2,3 → 1,3,2
+# 3,2,1 → 1,2,3
+# 1,1,5 → 1,5,1
+#
 
 #交换nums的i，j位置的元素
 def swap(nums, i, j):
@@ -65,7 +45,6 @@ def nextPermutation(nums):
         res = swap(nums,i,j)
     #把i+1位置起的元素全部反转
     return reverse(res,i+1)
-
 
 print(nextPermutation([1,2,3]))
 
--- ARRAY/不同路径.py
+++ ARRAY/不同路径.py
@@ -1,7 +1,28 @@
+
+# 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+#
+# 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+#
+# 问总共有多少条不同的路径？
+#
+# 示例 1:
+#
+# 输入: m = 3, n = 2
+# 输出: 3
+# 解释:
+# 从左上角开始，总共有 3 条路径可以到达右下角。
+# 1. 向右 -> 向右 -> 向下
+# 2. 向右 -> 向下 -> 向右
+# 3. 向下 -> 向右 -> 向右
+# 示例 2:
+#
+# 输入: m = 7, n = 3
+# 输出: 28
+
 """
 思路：动态规划
 我们令dp[i][j]是到达坐标点i,j的最多路径
-动态方程：dp[i][j] = dp[i-1][j] + dp[i][j-1]
+动态方程：dp[i][j] = dp[i-1][j] + dp[i][j-1](要到(i,j)，要么从(i-1,j)往下走一格,要么从(i,j-1)往右走一格
 注意：对于第一行dp[0][j] 或者第一列dp[i][0] 由于都是在边界，所以只能为1
 时间复杂度O(m*n)
 空间复杂度O(m*n)
@@ -23,36 +44,36 @@ import numpy as np
 # a = Solution()
 # print(a.uniquePaths(3,2))
 
-#空间复杂度优化到O(2n)
-import numpy as np
-# class Solution:
-#     def uniquePaths(self,m,n):
-#         # dp = np.zeros([m,n],np.int)
-#         pre = np.ones(n,np.int)
-#         cur = np.ones(n,np.int)
-#         # for i in range(n):#边界
-#         #     dp[0][i] = 1
-#         # for i in range(n):#边界
-#         #     dp[i][0] = 1
-#         for i in range(1,m):
-#             for j in range(1,n):#从上面来有多少条路+从左边来有多少条路 = 到这里有多少条路
-#                 cur[j] = cur[j-1] + pre[j]
-#             pre = cur.copy()
-#         return pre[n-1]
-#
-# a = Solution()
-# print(a.uniquePaths(3,2))
 
-#空间复杂度优化到O(n)
+# 优化一：将一个表优化成了表中我们需要的两行
+#空间复杂度优化到O(2n)
 import numpy as np
 class Solution:
     def uniquePaths(self,m,n):
-
-        cur = np.ones(n,np.int)
+        pre = np.ones(n,np.int)# pre为长度n初始值均为1的数组
+        cur = np.ones(n,np.int)# cur为长度n初始值均为1的数组
         for i in range(1,m):
-            for j in range(1,n):#从上面来有多少条路+从左边来有多少条路 = 到这里有多少条路
-                cur[j] += cur[j-1]
-        return cur[n-1]
+            for j in range(1,n):
+                #cur[j-1]是从左边来的,pre[j]是从上面来的
+                cur[j] = cur[j-1] + pre[j]
+            pre = cur.copy()
+        return pre[n-1]
 
 a = Solution()
 print(a.uniquePaths(3,2))
+
+# 优化二：在优化一的基础上，将两行优化成了我们需要的当前行，因为cur未更新前保存的结果是上一行的结果
+# #空间复杂度优化到O(n)
+# import numpy as np
+# class Solution:
+#     def uniquePaths(self,m,n):
+#         cur = np.ones(n,np.int)
+#         for i in range(1,m):
+#             for j in range(1,n):
+                  # 等号右边的cur[j]是上一行的第j个数据对应的步数,cur[j-1]是本行第j-1个数据对应的步数
+#                 # 等号右边的cur[j]是从上面来,等号右边的cur[j-1]是从左边来
+#                 cur[j] = cur[j] + cur[j-1]
+#         return cur[n-1]
+
+# a = Solution()
+# print(a.uniquePaths(3,2))
