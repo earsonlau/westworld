39e03071abd79caa110515912fc4d1aaa632211c
earsonlau <earsonlau@gmail.com>
2020-05-01 11:04:27 +0800

    增加python题解

--- LIST/移除链表元素.py
+++ LIST/移除链表元素.py
@@ -14,69 +14,51 @@
 # 添加一个虚拟头结点，删除头结点就不用另做考虑
 # 递归
 #
-# 方法一（删除头结点时另做考虑）
-# 1
-#
-#
-# class Solution {
-# 2        public ListNode removeElements(ListNode head, int val) {
-# 3 // 删除值相同的头结点后，可能新的头结点也值相等，用循环解决
-# 4
-#
-#
-# while (head != null & & head.val == val){
-# 5                head=head.next;
-# 6 	}
-# 7 	 if(head==null ) 8 	            return
-# head;
-# 9 	        Li
-# tNod e prev=head;
-# 10 	        //确保当前结点后还有结点
-# 11 	        while(pre v. next!=null){
-# 12 	            if(prev.ne xt .val==val){
-# 13 	                prev.next=prev.next.next;
-# 14 	            }else{
-# 15 	                prev=prev.next;
-# 16 	            }
-# 17 	        }
-# 18 	        return head;
-# 19 	}
-# 20 	}
-#
-# 方法二（添加一个虚拟头结点）
-# 1 	class Solution {
-# 2 	    public ListNode removeElements(ListNode head, int val) {
-# 3 	        //创建一个虚拟头结点
-# 4 	        ListNode dummyNode=new ListNode(val-1);
-# 5 	        dummyNode.next=head;
-# 6 	        ListNode prev=dummyNode;
-# 7 	        //确保当前结点后还有结点
-# 8 	        w hile(prev. ne xt!=null){
-# 9 	            if(prev.next .v al==val){
-# 10 	                prev.next=prev.next.next;
-# 11 	            } else{
-# 12 	                prev=prev.next;
-# 13 	            }
-# 14 	        }
-# 15 	        return dummyNode.next;
-# 16 	    }17 	}
-#
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+# 删除头结点时另做考虑（由于头结点没有前一个结点）
+class Solution:
+    def removeElements(self,head,val):
+        # 删除值相同的头结点后,可能新的头结点也是值相等的,用循环解决
+        while (head is not None and head.val == val):
+            head = head.next
+        if head is None:
+            return head
+        prev = head
+        # 确保当前结点后还有结点
+        while (prev.next is not None):
+            if prev.next.val == val:
+                prev.next = prev.next.next
+            else:
+                prev = prev.next
+        return head
+
+# 方法二: 添加一个虚拟头结点
+class Solution:
+    def removeElements(self,head,val):
+        # 创建一个虚拟头结点
+        dummyNode = ListNode(val-1)
+        dummyNode.next = head
+        prev = dummyNode
+        # 确保当前结点后还有结点
+        while (prev.next != None):
+            if prev.next.val == val :
+                prev.next = prev.next.next
+            else:
+                prev = prev.next
+        return dummyNode.next
+
 # 方法三（递归）
 #
-# 1 	class Solution {
-# 2 	    public ListNode removeElements(ListNode head, int val) {
-# 3 	       if (h ead==null)
-# 4 	           return null;
-# 5 	        head . next=removeElements(head. next,val);
-# 6 	        if(hea d. val=
-# =val){
-#     7 	            return head.next;
-# 8 	 }else{
-# 9
-# return head;
-# 10 	        }
-# 11 	    }
-# 12 	}
-#
-#
-#
+class Solution:
+    def removeElements(self,head,val):
+        if head is None:
+            return None
+        head.next = self.removeElements(head.next,val)
+        if head.val == val:
+            return head.next
+        else:
+            return head
--- LIST/重排链表.py
+++ LIST/重排链表.py
@@ -14,32 +14,28 @@
 # 链表的缺点就是不能随机存储，当我们想取末尾元素的时候，只能从头遍历一遍，很耗费时间。第二次取末尾元素的时候，又得遍历一遍。
 # 所以先来个简单粗暴的想法，把链表存储到线性表中，然后用双指针依次从头尾取元素即可。
 
-# public void reorderList(ListNode head) {
-#     if (head == null) {
-#         return;
-#     }
-#     //存到 list 中去
-#     List<ListNode> list = new ArrayList<>();
-#     while (head != null) {
-#         list.add(head);
-#         head = head.next;
-#     }
-#     //头尾指针依次取元素
-#     int i = 0, j = list.size() - 1;
-#     while (i < j) {
-#         list.get(i).next = list.get(j);
-#         i++;
-#         //偶数个节点的情况，会提前相遇
-#         if (i == j) {
-#             break;
-#         }
-#         list.get(j).next = list.get(i);
-#         j--;
-#     }
-#     list.get(i).next = null;
-# }
 
+def reorderList(head):
+    if head is None:
+        return
+    # 存到list
+    list_ =[]
+    while(head is not None):
+       list_.append(head)
+       head = head.next
 
+    #头尾指针依次取元素
+    i = 0
+    j = len(list_) -1
+    while i < j:
+        list_[i].next = list_[j]
+        i += 1
+        # 偶数个节点的情况,会提前相遇
+        if i == j :
+            break
+        list[j].next = list[i]
+        j -= 1
+    list[i].next = None
 
 # 解法二 递归
 # 参考 这里。
@@ -66,6 +62,36 @@
 #
 # 然后总体的代码就是下边的样子
 #
+
+def reorderListHelper(head,len):
+    if len == 1 :
+        outTail = head.next
+        head.next = None
+        return outTail
+    if len == 2 :
+        outTail = head.next.next
+        head.next.next = None
+        return outTail
+    # 得到对应的尾节点,并且将头结点和尾节点之间的链表通过递归处理
+    tail = reorderListHelper(head.next,len - 2)
+    subHead = head.next
+    head.next = tail
+    outTail = tail.next
+    tail.next = subHead
+    return outTail
+
+
+def reorderList(head):
+    if head is None or head.next is None or head.next.next is None:
+        return
+    len = 0
+    h = head
+    # 求出节点数
+    while h is not None:
+        len += 1
+        h = h.next
+    reorderListHelper(head,len)
+
 # 解法三
 # 参考
 # 这里，主要是利用到一头一尾取元素的特性。
@@ -87,3 +113,40 @@
 # 第二步链表逆序的话，有迭代和递归的两种方式，迭代的话主要利用两个指针，依次逆转。
 # 第三步的话就很简单了，两个指针分别向后移动就可以。
 #
+
+
+def reorderList(head):
+    if head is None or head.next is None or head.next.next is None:
+        return
+    # 找中点,链表分成两个
+    slow = head
+    fast = head
+    while fast.next is not None and fast.next.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+    newHead = slow.next
+    slow.next = None
+
+    # 第二个链表倒置
+    newHead = reverseList(newHead)
+
+    #链表节点依次连接
+    while newHead is not None:
+        temp = newHead.next
+        newHead.next = head.next
+        head.next = newHead
+        head = newHead.next
+        newHead = temp
+
+def reverseList(head):
+    if head is None:
+        return None
+    tail = head
+    head = head.next
+    tail.next = None
+    while head is not None:
+        temp = head.next
+        head.next = tail
+        tail = head
+        head = temp
+    return tail
