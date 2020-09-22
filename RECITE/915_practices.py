f90c41ef401f39deb9230929d047db7649f75b44
earsonlau <earsonlau@gmail.com>
2020-09-22 11:02:43 +0800

    做一些题库的题目

--- RECITE/915_practices.py
+++ RECITE/915_practices.py
@@ -165,218 +165,265 @@
 #             numbers.discard(i*p)
 #     return numbers
 # # print(primes(1000))
-
-# 9. 用冒泡或选排对20个数据进行排序后输出，并给出每个元素原来的位置
-def bb_sort_nums(nums):
-    #bubble sort
-    I = []
-    for i in range(len(nums)):
-        I.append(i)
-    for i in range (len(nums)):
-        for j in range(len(nums)-1,i,-1):#j移动的方向和要和i相反才能实现后面的小数字冒泡冒到前面
-            if nums[j] < nums[j-1]:
-                nums[j-1],nums[j] = nums[j],nums[j-1]
-                I[j-1],I[j] = I[j],I[j-1]
-    print("sorted result:",nums)
-    print("original index",I)
-    return 1
-# nums = [1, 17, 19, 23, 29, 31, 2, 5, 7, 11, 1, 3, 37, 41, 43, 47]
-# bb_sort_nums(nums)
-
-#选排
-def select_sort_nums(nums):
-    I = []
-    for i in range(len(nums)):
-        I.append(i)
-    for i in range(len(nums)):
-        min = i
-        for j in range(i+1,len(nums)):
-           if (nums[j] < nums[min]):
-               min = j
-        if (min != i):
-            t = nums[i]
-            nums[i] = nums[min]
-            nums[min] = t
-            I[i],I[min] = I[min],I[i]
-    print("sorted result:",nums)
-    print("original index",I)
-    return 1
-
-# nums = [1, 17, 19, 23, 29, 31, 2, 5, 7, 11, 1, 3, 37, 41, 43, 47]
-# select_sort_nums(nums)
-
-# 10. 把20个元素读入数组，找最大值和最小值并输出值与下标。最后将数组各元素按从大到小重新排列，输出。
-def sort_sunny(nums):
-    max = nums[0]
-    min = nums[0]
-    max_index = 0
-    min_index = 0
-    for i in range(len(nums)):
-        if nums[i] > max:
-            max = nums[i]
-            max_index = i
-        if nums[i] < min:
-            min = nums[i]
-            min_index = i
-    print("max:",max,"index:",max_index)
-    print("min:",min,"index:",min_index)
-
-
-    for i in range (len(nums)):
-        for j in range(len(nums)-1,i,-1):#j移动的方向和要和i相反才能实现后面的小数字冒泡冒到前面
-            if nums[j] > nums[j-1]:
-                nums[j-1],nums[j] = nums[j],nums[j-1]
-    print("sorted result:",nums)
-
-# nums = [1, 17, 19, 23, 29, 31, 2, 5, 7, 11, 1, 3, 37, 41, 43, 47]
-# sort_sunny(nums)
-
-# 11. 编写程序，不用标准函数而把两个字符串连接。
-def connect_string(str1,str2):
-    return print(str1+str2)
-
-# 12. 编写程序，求字符串长度，并将长度打印。
-def string_length(str):
-    return print(len(str))
-
-# 13. 编写排序程序，用数组排20个输入的数。
-def sort_leaf():
-    pass
-
-# 14. 从键盘读取10个整型数字，去掉重复的，剩下的从大到小输出
-def remove_dup(nums):
-    nums.sort(reverse =True)
-    res = []
-    for i in nums:
-        if i in res:
-            continue
-        else:
-            res.append(i)
-    print(res)
-
-# nums = [1, 1, 1, 3, 9, 3, 2, 5, 7, 11, 1, 3, 37, 41, 43, 47]
-# remove_dup(nums)
-
-# 15. 写个函数，从键盘读个字符串反序存放，并在主函数中输入输出该字符串
-def reverse_string(str):
-    if str == "":
-        return str
-    else:
-        return reverse_string(str[1:])+str[0]
-# print(reverse_string("abcdef"))
-
-
-
-# 16. 输入5个大学生4门功课的成绩，然后求出：
-#     1. 每个人的总分
-#     2. 每门课程的总分
-#     3. 输入总分最高的学生的姓名和总分数
-def score():
-    nums = [[0] * 5 for row in range(4)]
-    for i in range(5):
-        for j in range(4):
-            nums[i][j] = input("plz input the ",i,"th student's",j,"th score:")
-    #now we get all the scores for everybody
-    student_score = []
-    for i in range(5):
-        student_score.append(sum(nums[i]))
-    lesson_score = []
-    for j in range(4):
-        lesson_score.append(sum(nums[j]))
-    max = student_score[0]
-    for i in range(len(student_score)):
-        if student_score[i] > max:
-            max = student_score[i]
-            index= i
-    print("max student score",max," ,name:",index)
-    return 1
-
-# 17. 编写一个函数，输入一个十进制数，输出相应的二进制、八进制、十六进制数。
-
-def dec2bin(num):
-    # dec2bin
-    l = []
-    if num < 0:
-        return '-' + dec2bin(abs(num))
-    while(1):
-        num,remainder = divmod(num,2)#模2，取一下余数并更新num为原来的1/2
-        l.append(str(remainder))#把余数存起来
-        if num == 0 :
-            return ''.join(l[::-1])#反转输出
-
-def dec2oct(num):
-    # dec2oct
-    l = []
-    if num < 0:
-        return '-' + dec2oct(abs(num))
-    while (1):
-        num, remainder = divmod(num, 8)  # 模2，取一下余数并更新num为原来的1/2
-        l.append(str(remainder))  # 把余数存起来
-        if num == 0:
-            return ''.join(l[::-1])  # 反转输出
-
-
-base = [str(x) for x in range(9)] + [chr(x) for x in range(ord('A'), ord('A') + 6)]
-def dec2hex(num):
-# dec2hex
-    l = []
-    if num < 0:
-        return '-' + dec2hex(abs(num))
-    while (1):
-        num, remainder = divmod(num, 16)  # 模2，取一下余数并更新num为原来的1/2
-        l.append(base[remainder])  # 把余数存起来
-        if num == 0:
-            return ''.join(l[::-1])  # 反转输出
-
-
-def dec_to_bin_oct_hex(num):
-    print('bin:', dec2bin(num))
-    print('bin:',oct = dec2oct(num))
-    print('bin:',hex = dec2hex(num))
-    return 1
-
-# 18. 用递归法，求n!
-def cal_n_step_product(n):
-    if n == 0:
-        return 1
-    return n*(cal_n_step_product(n-1))
-
-# print(cal_n_step_product(3))
-
-# 19. 写个函数，输出数n从右边开始的第k个数字的值
-def kth_num_from_right(n,k):
-    if k >= len(str(n)):
-        print("k is too large!")
-        return 0
-    for i in range(k):
-        res = n % 10
-        n = n // 10
-    return print(res)
-kth_num_from_right(1234567,4)
-
-
-# 20. 写个回答星期几的函数。函数三个参分别为年月日，输出“星期__”
-def what_the_day():
-    import calendar
-    from calendar import *
-    import datetime
-    y = input('请输入年份')
-    m = input('请输入月份')
-    d = input('请输入日')
-    lis = ['星期一','星期二','星期三','星期四','星期五','星期六','星期日',]
-    dic = dict(enumerate(lis))
-    if y.isdigit() and m.isdigit() and d.isdigit() and 1<=int(m)<=12 and 1<=int(d)<=31 :
-        w = weekday(int(y),int(m),int(d))
-        print(dic[w])
+#
+# # 9. 用冒泡或选排对20个数据进行排序后输出，并给出每个元素原来的位置
+# def bb_sort_nums(nums):
+#     #bubble sort
+#     I = []
+#     for i in range(len(nums)):
+#         I.append(i)
+#     for i in range (len(nums)):
+#         for j in range(len(nums)-1,i,-1):#j移动的方向和要和i相反才能实现后面的小数字冒泡冒到前面
+#             if nums[j] < nums[j-1]:
+#                 nums[j-1],nums[j] = nums[j],nums[j-1]
+#                 I[j-1],I[j] = I[j],I[j-1]
+#     print("sorted result:",nums)
+#     print("original index",I)
+#     return 1
+# # nums = [1, 17, 19, 23, 29, 31, 2, 5, 7, 11, 1, 3, 37, 41, 43, 47]
+# # bb_sort_nums(nums)
+#
+# #选排
+# def select_sort_nums(nums):
+#     I = []
+#     for i in range(len(nums)):
+#         I.append(i)
+#     for i in range(len(nums)):
+#         min = i
+#         for j in range(i+1,len(nums)):
+#            if (nums[j] < nums[min]):
+#                min = j
+#         if (min != i):
+#             t = nums[i]
+#             nums[i] = nums[min]
+#             nums[min] = t
+#             I[i],I[min] = I[min],I[i]
+#     print("sorted result:",nums)
+#     print("original index",I)
+#     return 1
+#
+# # nums = [1, 17, 19, 23, 29, 31, 2, 5, 7, 11, 1, 3, 37, 41, 43, 47]
+# # select_sort_nums(nums)
+#
+# # 10. 把20个元素读入数组，找最大值和最小值并输出值与下标。最后将数组各元素按从大到小重新排列，输出。
+# def sort_sunny(nums):
+#     max = nums[0]
+#     min = nums[0]
+#     max_index = 0
+#     min_index = 0
+#     for i in range(len(nums)):
+#         if nums[i] > max:
+#             max = nums[i]
+#             max_index = i
+#         if nums[i] < min:
+#             min = nums[i]
+#             min_index = i
+#     print("max:",max,"index:",max_index)
+#     print("min:",min,"index:",min_index)
+#
+#
+#     for i in range (len(nums)):
+#         for j in range(len(nums)-1,i,-1):#j移动的方向和要和i相反才能实现后面的小数字冒泡冒到前面
+#             if nums[j] > nums[j-1]:
+#                 nums[j-1],nums[j] = nums[j],nums[j-1]
+#     print("sorted result:",nums)
+#
+# # nums = [1, 17, 19, 23, 29, 31, 2, 5, 7, 11, 1, 3, 37, 41, 43, 47]
+# # sort_sunny(nums)
+#
+# # 11. 编写程序，不用标准函数而把两个字符串连接。
+# def connect_string(str1,str2):
+#     return print(str1+str2)
+#
+# # 12. 编写程序，求字符串长度，并将长度打印。
+# def string_length(str):
+#     return print(len(str))
+#
+# # 13. 编写排序程序，用数组排20个输入的数。
+# def sort_leaf():
+#     pass
+#
+# # 14. 从键盘读取10个整型数字，去掉重复的，剩下的从大到小输出
+# def remove_dup(nums):
+#     nums.sort(reverse =True)
+#     res = []
+#     for i in nums:
+#         if i in res:
+#             continue
+#         else:
+#             res.append(i)
+#     print(res)
+#
+# # nums = [1, 1, 1, 3, 9, 3, 2, 5, 7, 11, 1, 3, 37, 41, 43, 47]
+# # remove_dup(nums)
+#
+# # 15. 写个函数，从键盘读个字符串反序存放，并在主函数中输入输出该字符串
+# def reverse_string(str):
+#     if str == "":
+#         return str
+#     else:
+#         return reverse_string(str[1:])+str[0]
+# # print(reverse_string("abcdef"))
+#
+#
+#
+# # 16. 输入5个大学生4门功课的成绩，然后求出：
+# #     1. 每个人的总分
+# #     2. 每门课程的总分
+# #     3. 输入总分最高的学生的姓名和总分数
+# def score():
+#     nums = [[0] * 5 for row in range(4)]
+#     for i in range(5):
+#         for j in range(4):
+#             nums[i][j] = input("plz input the ",i,"th student's",j,"th score:")
+#     #now we get all the scores for everybody
+#     student_score = []
+#     for i in range(5):
+#         student_score.append(sum(nums[i]))
+#     lesson_score = []
+#     for j in range(4):
+#         lesson_score.append(sum(nums[j]))
+#     max = student_score[0]
+#     for i in range(len(student_score)):
+#         if student_score[i] > max:
+#             max = student_score[i]
+#             index= i
+#     print("max student score",max," ,name:",index)
+#     return 1
+#
+# # 17. 编写一个函数，输入一个十进制数，输出相应的二进制、八进制、十六进制数。
+#
+# def dec2bin(num):
+#     # dec2bin
+#     l = []
+#     if num < 0:
+#         return '-' + dec2bin(abs(num))
+#     while(1):
+#         num,remainder = divmod(num,2)#模2，取一下余数并更新num为原来的1/2
+#         l.append(str(remainder))#把余数存起来
+#         if num == 0 :
+#             return ''.join(l[::-1])#反转输出
+#
+# def dec2oct(num):
+#     # dec2oct
+#     l = []
+#     if num < 0:
+#         return '-' + dec2oct(abs(num))
+#     while (1):
+#         num, remainder = divmod(num, 8)  # 模2，取一下余数并更新num为原来的1/2
+#         l.append(str(remainder))  # 把余数存起来
+#         if num == 0:
+#             return ''.join(l[::-1])  # 反转输出
+#
+#
+# base = [str(x) for x in range(9)] + [chr(x) for x in range(ord('A'), ord('A') + 6)]
+# def dec2hex(num):
+# # dec2hex
+#     l = []
+#     if num < 0:
+#         return '-' + dec2hex(abs(num))
+#     while (1):
+#         num, remainder = divmod(num, 16)  # 模2，取一下余数并更新num为原来的1/2
+#         l.append(base[remainder])  # 把余数存起来
+#         if num == 0:
+#             return ''.join(l[::-1])  # 反转输出
+#
+#
+# def dec_to_bin_oct_hex(num):
+#     print('bin:', dec2bin(num))
+#     print('bin:',oct = dec2oct(num))
+#     print('bin:',hex = dec2hex(num))
+#     return 1
+#
+# # 18. 用递归法，求n!
+# def cal_n_step_product(n):
+#     if n == 0:
+#         return 1
+#     return n*(cal_n_step_product(n-1))
+#
+# # print(cal_n_step_product(3))
+#
+# # 19. 写个函数，输出数n从右边开始的第k个数字的值
+# def kth_num_from_right(n,k):
+#     if k >= len(str(n)):
+#         print("k is too large!")
+#         return 0
+#     for i in range(k):
+#         res = n % 10
+#         n = n // 10
+#     return print(res)
+# kth_num_from_right(1234567,4)
+#
+#
+# # 20. 写个回答星期几的函数。函数三个参分别为年月日，输出“星期__”
+# def what_the_day():
+#     import calendar
+#     from calendar import *
+#     import datetime
+#     y = input('请输入年份')
+#     m = input('请输入月份')
+#     d = input('请输入日')
+#     lis = ['星期一','星期二','星期三','星期四','星期五','星期六','星期日',]
+#     dic = dict(enumerate(lis))
+#     if y.isdigit() and m.isdigit() and d.isdigit() and 1<=int(m)<=12 and 1<=int(d)<=31 :
+#         w = weekday(int(y),int(m),int(d))
+#         print(dic[w])
 
 # 21. 求给定5个数的最大值
+def get_max(nums):
+    max = nums[0]
+    for i in range(len(nums)):
+        if i > max:
+            max = nums[i]
+    print('the max number is :',max)
+    return 1
+
+# get_max([1,2,3,4,5])
 
 
 # 22. 用递归的方式写程序，输入一个非负整数，输出这个数的逆序十进制数
+# def reverse_num(num):
+#     num = str(num)
+#     if len(num) == 1 :
+#         if num == '0' :
+#             return ''
+#         else:
+#             return num
+#     return reverse_num(num[1:])+num[0]
+#
+# print(reverse_num(12340))
 # 23. 设计下面两个函数：
 #     1. 函数readoctal()，读入八进制序列，转十进制正整数
+def readoctal(num):
+    if num <0:
+        return '-'+readoctal(abs(num))
+    # num0,remainder = divmod(num,8)
+    # res = num0*10+remainder
+    # return res
+    num = str(num)
+    sum = 0
+    for i in range(len(num)):
+        sum += int(num[len(num) - 1 - i]) * pow(8, i)
+    return sum
+print(readoctal(13))
+
 #     2. 函数writeoctal(), 将十进制正整数转换成相应的八进制数字序列，并打印出来
+
+# def dec2oct(num):
+#     # dec2oct
+#     l = []
+#     if num < 0:
+#         return '-' + dec2oct(abs(num))
+#     while (1):
+#         num, remainder = divmod(num, 8)  # 模8，取一下余数并更新num为原来的1/8
+#         l.append(str(remainder))  # 把余数存起来
+#         if num == 0:
+#             return ''.join(l[::-1])  # 反转输出
 # 24. 编程实现查找字符串s2在字符串s1中第一次出现的位置，若找到则返回位置，否则返回0
+def find_string(s1,s2):
+    for i in s2:
+        if i == s1[0]:
+
 # 25. 函数expand(s,t) 在把字符串s复制到字符串t时，将其中的换行符和制表符转换成可见的转义字符，即用\n表示换行符，用\t表示制表符，用指针实现。
 # 26. 写一个函数getint，它把输入的一串数字字符转换为整数。
 # def getint():
