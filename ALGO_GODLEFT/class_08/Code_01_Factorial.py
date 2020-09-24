ac202ca79da3baa486d549b93b0a9d8a129243db
earsonlau <earsonlau@gmail.com>
2020-09-24 09:03:50 +0800

    把左神算法课的一些代码进行整理 1.写注释 2.翻译成python

---
+++ ALGO_GODLEFT/class_08/Code_01_Factorial.py
@@ -0,0 +1,15 @@
+class Code_01_Factorial:
+    def get_factorial1(self,n):
+        if n == 1 :
+            return 1
+        else :
+            return n * self.get_factorial1(n-1)
+
+    def get_factorial2(self,n):
+        res = 1
+        for i in range(1,n+1):
+            res *= i
+        return res
+sol = Code_01_Factorial()
+print(sol.get_factorial1(3))
+print(sol.get_factorial2(3))
---
+++ ALGO_GODLEFT/class_08/Code_02_Hanoi.py
@@ -0,0 +1,24 @@
+class Solution:
+    def hanoi(self,n):
+        if n > 0:
+            self.func(n, n, "left","mid","right")
+    def func(self,rest,down,from_,help,to):
+        #参数解释：
+        # rest 是本次调用func需要执行搬运的盘子数(继续往下递归的次数-1)，若rest为1，则打印本次搬运的盘子id和起始位置终止位置
+        # down 是本次搬运的盘子id
+        # from 是起始位置
+        # help 是中间位置
+        # to 是终止位置
+        if rest == 1 :
+            print("move ",down,"from ",from_, "to", to )
+        else:
+            #三步走
+            #1. 把n-1个盘子放到mid上面
+            self.func(rest - 1,down - 1 ,from_,to,help)
+            #2. 把第n个盘子放到right上面
+            self.func(1,down,from_,help,to)
+            #3. 把mid上面的n-1个盘子放到right上面
+            self.func(rest - 1,down - 1,help,from_,to)
+
+sol = Solution()
+sol.hanoi(3)
