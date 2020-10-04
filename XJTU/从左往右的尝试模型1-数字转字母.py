cb7fc28d1cf7dfd99476edff0dda517fec664c5f
earsonlau <earsonlau@gmail.com>
2020-10-04 20:07:53 +0800

    对"数字转字母"问题进行递归改dp

--- XJTU/从左往右的尝试模型1-数字转字母.py
+++ XJTU/从左往右的尝试模型1-数字转字母.py
@@ -29,4 +29,30 @@ def process(str,i):
     if str[i] - '0' >= 3:# str[i] == '3' ~ '9'
         res = process(str,i+1)
         return res
-print(convert_to_letter('111'))
+# print(convert_to_letter('111'))
+
+#改动态规划
+def dpway(string):
+    if len(string) == 0:
+        return 0
+    str= list(string)
+    N = len(str)
+    dp = [0]*(N+1)
+    dp[N] = 1
+    for i in range(N-1,-1,-1):
+        #str[i]不是'0'
+        if str[i] == '1':
+            dp[i] = dp[i+1]#i自己单独转化，后续有多少种方法
+            if i + 1 < len(str):
+                #i和i+1一起转化，后续有多少种方法
+                dp[i] += dp[i+2]
+
+        if str[i] == '2':
+            dp[i]= dp[i+1]
+            if str[i+1] - '0' <= 6:
+                dp[i]+=dp[i+2]
+
+        if int(str[i]) - int('0') >= 3:# str[i] == '3' ~ '9'
+            dp[i]=dp[i+1]
+    return dp[0]
+print(dpway("111"))
