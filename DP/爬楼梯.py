f7c03e9ea764ead58fa2107cae2c035f0f4a3e4d
earsonlau <earsonlau@gmail.com>
2020-10-13 19:41:59 +0800

    爬楼梯问题增加变形

--- DP/爬楼梯.py
+++ DP/爬楼梯.py
@@ -23,23 +23,86 @@
 #
 #  Related Topics 动态规划
 
+#
+#
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         """
+#         爬到第n楼的方法，为爬到第n-1楼和n-2楼的方法之和
+#         因为爬到n-1楼后，再爬1楼就能到达n楼
+#         爬到n-2楼同理
+#         因此只需初始化爬到1楼和爬到2楼分别有多少种方法，便可以推导出爬到n楼的方法
+#         """
+#         first = 1  # 爬到1楼只有1种方法
+#         second = 2  # 爬到2楼有两种方法
+#         res = 0  # 初始化爬到n楼的方法
+#         for i in range(3, n+1):  # 从3楼开始算
+#             res = first + second
+#             first = second  # 推导下一层  # 爬到 n - 2 楼的方法数赋值给first
+#             second = res # 爬到 n - 1 楼的方法数赋值给second
+#         return max(n, res)  # 返回n和res中较大的那个
+# print(Solution().climbStairs(4))
 
 
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        """
-        爬到第n楼的方法，为爬到第n-1楼和n-2楼的方法之和
-        因为爬到n-1楼后，再爬1楼就能到达n楼
-        爬到n-2楼同理
-        因此只需初始化爬到1楼和爬到2楼分别有多少种方法，便可以推导出爬到n楼的方法
-        """
-        first = 1  # 爬到1楼只有1种方法
-        second = 2  # 爬到2楼有两种方法
-        res = 0  # 初始化爬到n楼的方法
-        for i in range(3, n+1):  # 从3楼开始算
-            res = first + second
-            first = second  # 推导下一层  # 爬到 n - 2 楼的方法数赋值给first
-            second = res # 爬到 n - 1 楼的方法数赋值给second
-        return max(n, res)  # 返回n和res中较大的那个
-print(Solution().climbStairs(4))
 
+# 爬楼梯问题
+# 版本一：一次迈一步或两步
+def climbstair1(N):
+    dp = [None]*(N+1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2,N+1):
+       dp[i] = dp[i-1]+dp[i-2]
+    return dp[N]
+# print(climbstair1(3))
+# 版本二：一次迈1~k步
+def climbstair2(N,k):
+    dp = [0]*(N+1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2,N+1):
+        for j in range(1,k):
+            if i < k:
+                continue
+            dp[i] += dp[i-k]
+    return dp[N]
+# print(climbstair2(3,2))
+# 版本三：一次迈1~k步，有的台阶不能踩
+def climbstair3(N,k,stairs):
+    dp = [None]*(N+1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2,N+1):
+        for k in range(1,k):
+            if i < k :
+                continue
+            if stairs[i-1] == False:
+                dp[i] = 0
+            dp[i] = dp[i-1]+dp[i-2]
+    return dp[N]
+# print(climbstair3(3,2,[False,True,True]))
+# 版本四：一次迈1~2步，每个台阶有一个cost，最小化总的cost
+def climbstair4(N,p):
+    dp = [None]*(N+1)
+    dp[0] = 1
+    dp[1] = p[1]
+    for i in range(2,N+1):
+        dp[i] = min(dp[i-1],dp[i-2]) + p[i]
+    return dp[N]
+# print(climbstair4(3,[0,1,2,3]))
+# 版本五：一次迈1~2步，最小化cost，返回路径
+def climbstair5(N,p):
+    dp = [None]*(N+1)
+    dp[0] = 1
+    dp[1] = p[1]
+    path = []
+    for i in range(2,N+1):
+        dp[i] = min(dp[i-1],dp[i-2]) + p[i]
+        if dp[i-1] < dp[i-2]:
+            path.append(i-1)
+        else:
+            path.append(i-2)
+    path.append(N)
+    return path
+print(climbstair5(3,[0,1,2,3]))
+#
